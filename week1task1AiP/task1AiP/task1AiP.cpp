0b89d877777d7e6707040c90333ecb9386e351f6
angelinaserova <79716013+angelinaserova@users.noreply.github.com>
2021-04-05 16:55:46 +0300

    Add files via upload

---
+++ week1task1AiP/task1AiP/task1AiP.cpp
@@ -0,0 +1,15 @@
+﻿/*Вывести на экран Первую букву имени из символов "*" (звездочка)
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "      *       " << std::endl;
+    std::cout << "     * *      " << std::endl;
+    std::cout << "    *   *     " << std::endl;
+    std::cout << "   *******    " << std::endl;
+    std::cout << "  *       *   " << std::endl;
+    std::cout << " *         *  " << std::endl;
+    std::cout << "*           * " << std::endl;
+}
+
