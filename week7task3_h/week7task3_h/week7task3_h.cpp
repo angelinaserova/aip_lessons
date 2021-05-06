1ba5d3a9d34243bd46f20df4e961cc1eae09eaf5
angelinaserova <79716013+angelinaserova@users.noreply.github.com>
2021-05-07 01:26:09 +0300

    Add files via upload

---
+++ week7task3_h/week7task3_h/week7task3_h.cpp
@@ -0,0 +1,33 @@
+﻿// Пользователь задаёт число Ч и Х. Посчитать y: ЗАДАНИЕ H
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	float x, num;
+	float y = 0;
+	float chis = 1;
+	float znam = 1;
+	int fac = 0;
+	std::cout << "VVedite nechetnoe chislo\n";
+	std::cin >> num;
+	std::cout << "chislo=" << num << std::endl;
+
+	std::cout << "VVedite x\n";
+	std::cin >> x;
+	std::cout << "x=" << x << std::endl;
+
+	while (fac  <= num ) {
+		fac += 1;
+		chis=chis*x;
+		znam =znam*fac;
+		if (fac % 2 != 0) {
+			y += chis / znam;
+			y = y * (-1);
+		}
+	}
+
+	std::cout << "y=" << y;
+}
---
+++ week7task4/week7task4/week7task4.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, m, h;
+	cout << "введите начальное число\n";
+	cin >> n;
+	cout << "n=" << n << "\n";
+	cout << "введите конечное число\n";
+	cin >> m;
+	cout << "m=" << m << "\n";
+	cout << "введите шаг\n";
+	cin >> h;
+	cout << "h=" << h << "\n";
+
+	while (n <= m) {
+		cout << n << " ";
+		n += h;
+	}
+}
